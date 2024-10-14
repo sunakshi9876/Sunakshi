7a67cffa7eb95c8256b800ccf8109579eb24be63
sunakshi9876 <sunakshi984@gmail.com>
2024-10-14 19:06:12 +0530

    Day6

---
+++ Day6/Script.js
@@ -0,0 +1,39 @@
+// let a=()=>{
+//     console.log("Arrow Function")
+// }
+// let b=()=>console.log("Arrow Function2")
+// a()
+// b()
+// let c=(x,y)=>{
+//     return x+y
+// }
+// let res=c("Rahul","Ravi")
+// let res1=c(12,32)
+// console.log(res,res1)
+
+// function display(){
+//     // let p1=document.querySelector("p")
+//     // let p1=document.querySelectorAll("p")
+//     // p1[0].innerHTML="I am in parra"
+//     // p1[0].style.backgroundColor="red"
+//     // p1[1].innerHTML="I am in 2nd parra"
+//     // p1[1].style.backgroundColor="green"
+    
+// }
+
+// function changeHeader(){
+//     let h1=document.querySelector("h1")
+//     h1.textContent="Change Header"
+//     let a1=document.querySelector("a")
+//     a1.setAttribute("href","https://aktu.ac.in")
+//     a1.innerHTML="Aktu Website"
+// }
+
+// let create=()=>{
+//     const newDiv=document.createElement("div")
+//     newDiv.textContent="This is new div"
+
+//     const body=document.querySelector("body")
+//     body.appendChild(newDiv)
+// }
+
